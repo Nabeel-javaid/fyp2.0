26a980bb1aca79c34194f6d7c0a5006ecda9442f
Lowkey <i202420@nu.edu.pk>
2023-10-24 12:21:28 +0500

    added user profile and updated view market

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CreateMarket from "./pages/CreateMarket";
-import ViewMarket from './pages/ViewMarket';
-import ViewM from './pages/ViewM';
+import ViewMarkets from './pages/ViewMarkets';
 import CreateLoan from './pages/CreateLoan';
 import ViewLoan from './pages/ViewLoan';
+import UserProfile from './pages/UserProfile';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element=<HomePage /> />
-        <Route path="/view-market" element=<ViewMarket /> />
-        <Route path="/create-market" element=<CreateMarket /> />
-        <Route path="/view-loan" element=<ViewLoan /> />
-        <Route path="/create-loan" element=<CreateLoan /> />
-        <Route path="/view-m" element=<ViewM /> />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/view-market" element={<ViewMarkets />} />
+        <Route path="/create-market" element={<CreateMarket />} />
+        <Route path="/view-loan" element={<ViewLoan />} />
+        <Route path="/create-loan" element={<CreateLoan />} />
+        <Route path="/profile" element={<UserProfile />} />
       </Routes>
     </BrowserRouter>
 
