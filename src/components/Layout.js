6e9bf03d4cd9141ee8ffc75414b8b7224d379514
MuhammadAhmed-1855 <i201855@nu.edu.pk>
2023-12-11 23:22:39 +0500

    Adding Vercel Speed Insights

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,14 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
-
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const Layout = ({ children }) => {
     return (
         <div>
             <NavBar />
             {children}
-           
+            <SpeedInsights/>
             <Footer />
         </div>
     );
